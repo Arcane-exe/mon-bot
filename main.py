eefb04ccd7c683c6055fd36ae7a12f04f80c9bde
Arcane-exe <elijosh974@gmail.com>
2026-09-06 17:33:26 +0200

    Update Nitro generator script
    Refactor code to improve readability and fix file naming.

--- main.py
+++ main.py
@@ -1,35 +1,17 @@
 import random, string
 
-arcane = '''
-
-                                          _____  _____ _   _  _   _ _____ ___________ _____ 
-                                        |  __ \|  ___| \ | || \ | |_   _|_   _| ___ \  _  |
-                                        | |  \/| |__ |  \| ||  \| | | |   | | | |_/ / | | |
-                                        | | __ |  __|| . ` || . ` | | |   | | |    /| | | |
-                                        | |_\ \| |___| |\  || |\  |_| |_  | | | |\ \\ \_/ /
-                                        \____/\____/\_| \_/\_| \_/\___/  \_/ \_| \_|\___/ 
-                                                       By Arcane                             
-                                                                                                     
-'''
-print("Bot lancé")
-
-
-
-
-
-
-
-
-
-
-
-
-amount = int(input('Nombre de nitros à generer: '))
+print('Bienvenue dans le gen de >"Arcane\n')
+amount = int(input('Combien de nitros UNCHECK veux-tu générer ?: '))
 value = 1
-while value <= amount: 
+while value <= amount:
     code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
-    f = open('Codes.txt', "a+")
+    f = open('codes.txt', "a+")
     f.write(f'{code}\n')
     f.close()
-    print(f'Générer | {code}')
-    value +=1
+    print(f'[GEN BY  >"Arcane] {code}')
+    value += 1
+
+print('\n') ; print(f'dis moi merci, tu viens de générer {value -1} nitros uncheck\n ') ; print('Les codes ont été sauvegardé dans fichier ce nomant codes.txt\n')
+print('by >"Arcane\n')
+
+exit()
