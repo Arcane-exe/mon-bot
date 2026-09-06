b77f6734fc8b294b1f4c8d2793fbaadd49d3c5b7
Arcane-exe <elijosh974@gmail.com>
2026-09-06 17:40:34 +0200

    Fix while loop condition and increment value

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import random, string
 print('Bienvenue dans le gen de >"Arcane\n')
 amount = print('Combien de nitros UNCHECK veux-tu générer ?: ')
 value = 1
-while value = amount:
+while value = amount
     code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
     f = open('codes.txt', "a+")
     f.write(f'{code}\n')
     f.close()
     print(f'[GEN BY  >"Arcane] {code}')
-    value = 1
+    value += 1
 
 print('\n') ; print(f'dis moi merci, tu viens de générer {value -1} nitros uncheck\n ') ; print('Les codes ont été sauvegardé dans fichier ce nomant codes.txt\n')
 print('by >"Arcane\n')
